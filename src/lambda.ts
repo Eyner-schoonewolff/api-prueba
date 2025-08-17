2c1601bceb52990d9f606a13837ae7959957cc6f
Eyner-schoonewolff <enewolff2013@gmail.com>
2025-08-17 14:56:37 -0500

    fix(api): configuracion de documentacion swagger

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as serverless from 'serverless-http';
 
 let handler: any;
@@ -7,11 +9,23 @@ let handler: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Configurar CORS si es necesario
+  // Configurar pipes globales
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+  
+  // Configurar CORS
   app.enableCors({
     origin: true,
     credentials: true,
   });
+
+  // Configurar Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Wompi API')
+    .setDescription('API para prueba tecnica Wompi')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.init();
   
